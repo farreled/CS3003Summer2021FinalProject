a44db1528906a99b152febf99e32b35a3fcbdf9c
P0NYB0Y <will.a.schreiner@gmail.com>
2021-08-05 16:17:53 -0400

    Added new test parts

--- CS3003Summer2021FInalProject/test.cpp
+++ CS3003Summer2021FInalProject/test.cpp
@@ -6,11 +6,20 @@ int main() {
     float float_val_b = .3f;
     float div_mult_result;
     bool test_bool = false;
+    double test_new_operatorA = .05;
+    double test_new_operatorB = .07;
+    double test_new_operatorC;
 
     add_sub_result = int_val_a + int_val_b;
     add_sub_result = int_val_a - int_val_b;
     div_mult_result = float_val_a * float_val_b;
     div_mult_result = float_val_a / float_val_b;
+    
+    test_new_operatorC = test_new_operatorA +* test_new_operatorB;
+
+    until (int_val_a >= 10) {
+        int_val_a = int_val_a + 1;
+    }
 
     if (int_val_a > 2) {
         test_bool = true;
